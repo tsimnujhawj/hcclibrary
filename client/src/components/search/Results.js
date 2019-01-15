134d43bf378644da95bd22d06f93cf71bf2f191d
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-01-14 19:10:09 -0600

    added loading gif and fixed css for result component

--- client/src/components/search/Results.js
+++ client/src/components/search/Results.js
@@ -23,25 +23,33 @@ class Results extends Component {
   
   render () {
     var { isLoaded, items } = this.state;
+    if (!isLoaded) {
+      return (
+        <img className="loading-icon" src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif"/>
+      )
+    } else {
     return (
       <div>
-        {items.map(item => (
-  <div className="container result-container">
-  <div className="list-group result-item">
-  <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
-    <div className="d-flex w-100 justify-content-between">
-      {/* <h5 className="mb-1">{props.searchQuery}</h5> */}
-      <small>3 days ago</small>
-    </div>
-    {/* <div>{props.topicQuery} | {props.topicId}</div> */}
-    <p className="mb-1" key={item.id}>{item.item_title}</p>
-  </a>
-    </div>
-  </div>
-  ))}
-  </div>
+        
+          <div className="container result-container">
+          {items.map(item => (
+          <div className="list-group result-item">
+          <a href="#" className="list-group-item list-group-item-action flex-column align-items-start active">
+            <div className="d-flex w-100 justify-content-between">
+              <h5 className="mb-1" key={item.id}>{item.item_title}</h5>
+              <small>3 days ago</small>
+            </div>
+            <p className="mb-1 author-name">{item.item_author}</p>
+            <p className="mb-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu dapibus ex.</p>
+          </a>
+            </div>
+            ))}
+          </div>
+
+      </div>
     )
   }
+}
 };
 
 export default Results;
