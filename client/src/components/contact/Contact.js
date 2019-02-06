c00d0fee90772b7a108c61faa9b818eb7080b638
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-06 12:39:27 -0600

    update to landing page, news and updates

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -14,7 +14,7 @@ const Contact = props => (
            <h1 id="hero-title">Contact Us</h1>
         </div>
         <br/>
-        <div className="container">
+        <div className="container form-container">
             <div className="row">
             <div className="col-6">
             <h5>Hmong Cultural Center</h5>
@@ -30,7 +30,7 @@ const Contact = props => (
                     <br/>
                     E-Mail: txong@hmongcc.org</p>
             </div>
-            <div className="col-md-6 col-sm-12 form-container">
+            <div className="col-md-6 col-sm-12">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.394245951151!2d-93.11794628440367!3d44.95598087909832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62aa05af00001%3A0xd24f8f35bc5622d7!2s375+University+Ave+W%2C+St+Paul%2C+MN+55103!5e0!3m2!1sen!2sus!4v1549401573538" style={style}></iframe>
             </div>
             </div>
--- client/src/components/layouts/HomeLanding.js
+++ client/src/components/layouts/HomeLanding.js
@@ -10,7 +10,33 @@ const sectionStyle = {
 
 const HomeLanding = () => {
         return (
-            <div style={sectionStyle} className="landing-image"></div>
+            <div style={sectionStyle} className="landing-image">
+
+            <div className="news-container">
+            <div className="container search-intro">
+            <h2 className="text-center">Introduction</h2>
+            {/* <h6 className="text-center">02/06/2019</h6> */}
+            <div className="text-center">
+                <p>Welcome to the Hmong Resource Center Library!</p><p>We have the most comprehensive collection of Hmong-related literature, scholarly research, and multimedia materials in North America.
+                Hmong Cultural Center is the primairy Hmong and Asian-American organization in Minneapolis-Saint Paul, Minnesota that provides community outreach activities related to multicultural education for the purposes of promoting positive race relations in the Twin Cities community. Without the generous donations and grants from our donars and folks like you, we could not exist as a center for the community.</p>
+                <p>Please come <a href="/contact">take a tour</a> of our exhibit and consider <a href="https://www.givemn.org/organization/Hmong-Cultural-Center" target="_blank">donating</a>.</p>
+                <p>Thank you for visiting!</p>
+            </div>
+            </div>
+            </div>
+
+            <div className="news-container">
+            <div className="container search-intro">
+            <h2 className="text-center">Information regarding the library search</h2>
+            <h6 className="text-center">
+            <p className="text-center">
+              Please be aware that the catalog is in development.<br/>All features may not be available.
+            </p>
+            </h6>
+            </div>
+            </div>
+
+            </div>
         )
 }
 
