bb4f8e7a8433ccb5034e1afec5a69ea0412c2b46
tsimnujhawj <tsimnuj.hawj@gmail.com>
2019-02-06 11:12:26 -0600

    resized embedded map

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -3,11 +3,12 @@ import HccEmblem from "../../images/hcc_emblem.png"
 
 const About = props => (
     <div>
+        <div className="hero-content">
+           <h1 id="hero-title">About Us</h1>
+        </div>
+        <br/>
         <div className="container">
-        <br/>
-        <h1>About Us</h1>
-        <br/>
-        <img src={HccEmblem}/>
+        <img className="image-center" src={HccEmblem}/>
         <br/>
         <p>Hmong Cultural Center is the primary Hmong and Asian American organization in the Minneapolis-St. Paul area that provides community outreach activities related to multicultural education for the purposes of promoting positive race relations in the Twin Cities community.</p>
         <p>The Hmong Cultural Center is home to the <strong>Hmong Resource Center Library</strong>, which is perhaps the most comprehensive collection of Hmong-related literature, scholarly research, and multimedia materials in North America. The Hmong Cultural Center Museum offers many interactive displays that teach about the Hmong people, their history, their culture and their experience in the U.S. over the past 25 years. Please visit our center if you live in the Twin Cities or are planning on visiting Minneapolis-St. Paul.</p>
--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -2,18 +2,22 @@ import React from "react";
 
 let style = {
     border: "0px",
-    minWidth: "600px",
-    minHeight: "450px"
+    // minWidth: "300px",
+    // minHeight: "225px",
+    width: "100%",
+    height: "450px"
 }
 
 const Contact = props => (
     <div>
+        <div className="hero-content">
+           <h1 id="hero-title">Contact Us</h1>
+        </div>
+        <br/>
         <div className="container">
-        <br/>
-        <h1>Contact Us</h1>
-        <br/>
-            
-                <h5>Hmong Cultural Center</h5>
+            <div className="row">
+            <div className="col-6">
+            <h5>Hmong Cultural Center</h5>
                     <p>375 University Avenue, Suite 204
                     <br/>    
                     Saint Paul, MN 55103
@@ -25,9 +29,11 @@ const Contact = props => (
                     Executive Director: Txongpao Lee
                     <br/>
                     E-Mail: txong@hmongcc.org</p>
-
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.394245951151!2d-93.11794628440367!3d44.95598087909832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62aa05af00001%3A0xd24f8f35bc5622d7!2s375+University+Ave+W%2C+St+Paul%2C+MN+55103!5e0!3m2!1sen!2sus!4v1549401573538" allowFullScreen style={style}></iframe>
-
+            </div>
+            <div className="col-md-6 col-sm-12">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2823.394245951151!2d-93.11794628440367!3d44.95598087909832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87f62aa05af00001%3A0xd24f8f35bc5622d7!2s375+University+Ave+W%2C+St+Paul%2C+MN+55103!5e0!3m2!1sen!2sus!4v1549401573538" style={style}></iframe>
+            </div>
+            </div>
         </div>
     </div>
 )
